3865fe364d5387b75f4f9bb371873b8118e0450d
Nikhil M Jeby <nikhilmjeby.mec@gmail.com>
2026-05-01 00:46:14 +0530

    Initial commit

---
+++ src-tauri/gen/android/app/src/main/java/com/nikhi/super_note/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nikhi.super_note
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
